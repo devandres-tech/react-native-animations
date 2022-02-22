303f698ed3c2a7108aef2f7feda8103d7fcb52fa
andres alcocer <devandres.tech@gmail.com>
2022-02-22 10:45:19 -0500

    created carousel animation

---
+++ App.tsx
@@ -0,0 +1,9 @@
+// Inspiration: https://dribbble.com/shots/14139308-Simple-Scroll-Animation
+// Illustrations by: SAMji https://dribbble.com/SAMji_illustrator
+
+import * as React from 'react'
+import { Carousel } from './animations'
+
+export default () => {
+  return <Carousel />
+}

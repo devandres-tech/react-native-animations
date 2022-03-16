8ac9482f3f2dc16766aaabd0cb2c8a526020882c
andres alcocer <devandres.tech@gmail.com>
2022-03-16 09:26:28 -0400

    created bottom sheet animation

--- animations/BottomSheet.tsx
+++ animations/BottomSheet.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react'
 import {
   Image,
-  FlatList,
   View,
   StatusBar,
   Dimensions,
   StyleSheet,
+  Animated,
+  Text,
 } from 'react-native'
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet'
 
 const { width, height } = Dimensions.get('screen')
 
 const ITEM_WIDTH = width
 const ITEM_HEIGHT = height * 0.75
+const DOT_SIZE = 8
+const DOT_SPACING = 8
+const DOT_INDICATOR_SIZE = DOT_SIZE + DOT_SPACING
 
 const images = [
   'https://static.zara.net/photos///2020/I/1/1/p/6543/610/091/2/w/2460/6543610091_1_1_1.jpg?ts=1606727905128',
@@ -31,11 +36,110 @@ const product = {
 }
 
 export default () => {
+  const scrollY = React.useRef(new Animated.Value(0)).current
+
+  const handleSheetChange = React.useCallback((index) => {
+    console.log('handleSheetChange', index)
+  }, [])
+
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <StatusBar hidden />
+      <View style={{ height: ITEM_HEIGHT, overflow: 'hidden' }}>
+        <Animated.FlatList
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            {
+              useNativeDriver: true,
+            }
+          )}
+          data={images}
+          decelerationRate='fast'
+          snapToInterval={ITEM_HEIGHT}
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                <Image source={{ uri: item }} style={styles.image} />
+              </View>
+            )
+          }}
+        />
+        <View style={styles.pagination}>
+          {images.map((_, index) => {
+            return <View key={index} style={[styles.dot]} />
+          })}
+          <Animated.View
+            style={[
+              styles.dotIndicator,
+              {
+                transform: [
+                  {
+                    translateY: Animated.divide(
+                      scrollY,
+                      ITEM_HEIGHT
+                    ).interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, DOT_INDICATOR_SIZE],
+                    }),
+                  },
+                ],
+              },
+            ]}
+          />
+        </View>
+      </View>
+      <BottomSheet
+        snapPoints={[height - ITEM_HEIGHT, height]}
+        onChange={handleSheetChange}
+      >
+        <BottomSheetScrollView
+          style={{ backgroundColor: 'white' }}
+          contentContainerStyle={{ padding: 20 }}
+        >
+          <Text style={{ fontWeight: '800', fontSize: 16 }}>
+            {product.title}
+          </Text>
+          <Text style={{ fontSize: 16 }}>{product.price}</Text>
+          <View style={{ marginVertical: 16 }}>
+            {product.description.map((text, index) => {
+              return <Text key={index}>{text}</Text>
+            })}
+          </View>
+        </BottomSheetScrollView>
+      </BottomSheet>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+    resizeMode: 'cover',
+  },
+  pagination: {
+    position: 'absolute',
+    top: ITEM_HEIGHT / 2,
+    left: 20,
+  },
+  dot: {
+    width: DOT_SIZE,
+    height: DOT_SIZE,
+    borderRadius: DOT_SIZE,
+    backgroundColor: '#333',
+    marginBottom: DOT_SPACING,
+  },
+  dotIndicator: {
+    width: DOT_INDICATOR_SIZE,
+    height: DOT_INDICATOR_SIZE,
+    borderRadius: DOT_INDICATOR_SIZE,
+    borderWidth: 1,
+    borderColor: '#333',
+    position: 'absolute',
+    top: -DOT_SIZE / 2,
+    left: -DOT_SIZE / 2,
+  },
+})
